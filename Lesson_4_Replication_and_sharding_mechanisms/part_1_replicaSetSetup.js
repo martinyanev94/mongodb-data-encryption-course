f53618932fb9aaeca7f714bcc967deabb79e560c
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-08-10 03:48:03 -0400

    Add Lesson_4_Replication_and_sharding_mechanisms/part_1_replicaSetSetup.js

---
+++ Lesson_4_Replication_and_sharding_mechanisms/part_1_replicaSetSetup.js
@@ -0,0 +1,8 @@
+rs.initiate({
+    _id: "rs0",
+    members: [
+        { _id: 0, host: "primary:27017" },
+        { _id: 1, host: "secondary1:27017" },
+        { _id: 2, host: "secondary2:27017" }
+    ]
+});
