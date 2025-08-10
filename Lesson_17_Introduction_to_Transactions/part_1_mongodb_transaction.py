b2e863148f4e43a9ed0ccbbb0f77bc94df02db64
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-08-10 03:45:45 -0400

    Add Lesson_17_Introduction_to_Transactions/part_1_mongodb_transaction.py

---
+++ Lesson_17_Introduction_to_Transactions/part_1_mongodb_transaction.py
@@ -0,0 +1,14 @@
+from pymongo import MongoClient
+
+client = MongoClient('mongodb://localhost:27017/')
+orders = client.db.orders
+inventory = client.db.inventory
+
+with client.start_session() as session:
+    with session.start_transaction():
+        orders.insert_one({"sku": "abc123", "qty": 100}, session=session)
+        inventory.update_one(
+            {"sku": "abc123", "qty": {"$gte": 100}},
+            {"$inc": {"qty": -100}},
+            session=session,
+        )
